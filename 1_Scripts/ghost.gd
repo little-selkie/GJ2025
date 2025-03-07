e17d714e5ed1bdba04f4dd879c090ddf2f534d55
Rin <rin.kramarenko@gmail.com>
2025-03-07 13:06:09 +0200

    Ghost Gym testing

--- 1_Scripts/ghost.gd
+++ 1_Scripts/ghost.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var is_patrolling : bool = true
 @export var ghost_speed: int = 100
 
-@onready var pathfollow = get_node("Path2D/PathFollow2D")
+@onready var pathfollow = get_parent()
 
 func _physics_process(delta):
 	if is_patrolling == true:
