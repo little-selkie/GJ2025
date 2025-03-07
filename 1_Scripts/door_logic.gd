8c726d16e75fc95071b08b65e1836a1eea53690b
Rin <rin.kramarenko@gmail.com>
2025-03-07 17:14:39 +0200

    door is here

--- 1_Scripts/door_logic.gd
+++ 1_Scripts/door_logic.gd
@@ -1,1 +1,22 @@
 extends StaticBody2D
+
+var is_player_near: bool = false
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("interact"):
+		if is_player_near:
+			open_door()
+
+func _on_area_of_interaction_area_entered(area: Area2D) -> void:
+	print("you can open")
+	is_player_near = true
+
+
+func _on_area_of_interaction_area_exited(area: Area2D) -> void:
+	print("you can't open")
+	is_player_near = false
+	
+func open_door() -> void:
+	get_node("CollisionShape2D").disabled = true
+	get_node("AreaOfInteraction").monitoring = false
+	self.modulate.a = 0
