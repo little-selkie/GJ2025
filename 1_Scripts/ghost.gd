b27fc105aa2e428a2b457545b2e4a824e95f6015
Rin <rin.kramarenko@gmail.com>
2025-03-07 15:16:02 +0200

    chasing ghost

--- 1_Scripts/ghost.gd
+++ 1_Scripts/ghost.gd
@@ -7,12 +7,12 @@ extends CharacterBody2D
 @onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
 @onready var pathfollow = get_parent()
 
-var target_to_hunt: CharacterBody2D
+@export var target_to_hunt: CharacterBody2D
 
 func _physics_process(delta):
 	if is_patrolling == true:
 		pathfollow.progress += ghost_speed * delta
 	if is_hunting == true:
 		navigation_agent_2d.target_position = target_to_hunt.global_position
-		velocity = global_position.direction_to(navigation_agent_2d.get_next_path_position()) * ghost_speed * delta
+		velocity = global_position.direction_to(navigation_agent_2d.get_next_path_position()) * ghost_speed * delta * 100
 		move_and_slide()
