58316f1d925b2f28ec91ff5ec6e58d5552db10ee
Ev1lVamp <khromykh94@gmail.com>
2025-03-07 14:55:00 +0200

    Bind SFX and background

--- 1_Scripts/main_menu.gd
+++ 1_Scripts/main_menu.gd
@@ -12,8 +12,17 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://0_Scenes/House.tscn")
-
+	get_tree().change_scene_to_file("res://0_Scenes/MainScene.tscn")
+	$VBoxContainer/Button/Click.play()
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+	$VBoxContainer/Button2/Click2.play()
+
+
+func _on_button_mouse_entered() -> void:
+	$VBoxContainer/Button/Hover.play()
+
+
+func _on_exit_mouse_entered() -> void:
+	$VBoxContainer/Button2/Hover2.play()
