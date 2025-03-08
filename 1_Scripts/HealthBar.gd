f79fd0d2db345737b9ecd60457b3ee028833c7e1
Ev1lVamp <khromykh94@gmail.com>
2025-03-08 23:07:24 +0200

    edit UI

--- 1_Scripts/HealthBar.gd
+++ 1_Scripts/HealthBar.gd
@@ -1,9 +1,12 @@
 extends CanvasLayer
 
 @onready var health_bar: TextureProgressBar = $HealthBar
+@onready var candle_level: TextureProgressBar = $CandleBar
 
 func _ready():
 	health_bar.value = GlobalVars.player_health
+	candle_level.value = GlobalVars.candle_level
 	
 func _process(delta):
 	health_bar.value = GlobalVars.player_health
+	candle_level.value = GlobalVars.candle_level
