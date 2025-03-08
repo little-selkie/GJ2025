bf84d66eb13abfe4cb1452b115eaa9322fa998dd
Ev1lVamp <khromykh94@gmail.com>
2025-03-08 12:53:15 +0200

    MainMenu-Scene transition

--- 1_Scripts/main_menu.gd
+++ 1_Scripts/main_menu.gd
@@ -1,19 +1,17 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var transition = $Transition
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://0_Scenes/MainScene.tscn")
 	$VBoxContainer/Button/Click.play()
+	transition.play("fade_out")
+
+func _on_transition_animation_finished(_anim_name):
+	get_tree().change_scene_to_file("res://0_Scenes/MainScene.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
---
+++ 1_Scripts/main_scene.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+@onready var transition = $Transition
+
+func _ready():
+	transition.play("fade_in")
