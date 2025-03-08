f8f1ffb8a0d7da239b35e9c207e442471b12e1c1
Ev1lVamp <khromykh94@gmail.com>
2025-03-08 21:51:33 +0200

    Add scenes

--- 1_Scripts/main_scene.gd
+++ 1_Scripts/main_scene.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
-@onready var transition = $Transition
+
 
 var already_started: bool = false
 
-func _ready():
-	transition.play("fade_in")
+
 
 func _process(delta: float) -> void:
 	if GlobalVars.is_exorcism_available and !already_started:
